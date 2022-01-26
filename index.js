6c2f95b20c612ce094b4461730fa035148ad87e6
Menegoth <antonioareas@me.com>
2022-01-25 23:05:35 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your Code Here
+let userName = window.prompt("Please enter your name: ");
+let userScore = 0;
+let playAgain = true;
+
+//ask questions function
+function PlayGame() {
+    for (let i = 0; i < questions.length; i++) {
+
+        let question = questions[i];
+        let userAnswer = window.prompt(question.text);
+    
+        if (userAnswer.toUpperCase() === question.correctAnswer) {
+            userScore += 10;
+        }
+    
+    }
+    
+    window.alert(`Your sccore is: ${userScore}`);
+}
+
+//loop until user ends
+while (playAgain === true) {
+
+    PlayGame();
+    let userChoice = window.prompt("Would you like to play the game again? Answer yes or no.");
+
+    if (userChoice.toLowerCase() === "yes") {
+        playAgain = true;
+    } else {
+        playAgain = false;
+        window.alert("Thanks for playing the game.");
+    }
+
+}
